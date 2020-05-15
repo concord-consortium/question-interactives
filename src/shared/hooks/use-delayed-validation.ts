87dc380d149959008ad049ec2ed00a135b6f4d09
pjanik <janikpiotrek@gmail.com>
2020-05-15 18:18:17 +0200

    Add useDelayedValidation hook
    [#172209205]

---
+++ src/shared/hooks/use-delayed-validation.ts
@@ -0,0 +1,20 @@
+import { RefObject, useRef } from "react";
+import Form from "react-jsonschema-form";
+
+interface IConfig {
+  formRef: RefObject<Form<any>>;
+  delay?: number; // ms
+}
+
+const defaultDelay = 500; // ms
+
+// Note that this will actually trigger submit(), as that's the way to trigger validation.
+// Our form don't have submit buttons and don't set onSubmit handlers, so it should be fine.
+export const useDelayedValidation = (config: IConfig) => {
+  const timer = useRef<number>();
+
+  return () => {
+    window.clearTimeout(timer.current);
+    timer.current = window.setTimeout(() => config.formRef.current?.submit(), config.delay || defaultDelay);
+  };
+}
