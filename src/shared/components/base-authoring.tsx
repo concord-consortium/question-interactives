a7c56f1f25fdf03ea4f3c2a6f6a7f17a02e7944a
pjanik <janikpiotrek@gmail.com>
2020-05-27 19:18:15 +0200

    Add optional validation to BaseAuthoring
    [#172821069]

--- src/shared/components/base-authoring.tsx
+++ src/shared/components/base-authoring.tsx
@@ -1,6 +1,7 @@
-import React from "react";
-import Form, { Field, IChangeEvent, UiSchema } from "react-jsonschema-form";
+import React, { useEffect, useRef } from "react";
+import Form, { Field, FormValidation, IChangeEvent, UiSchema } from "react-jsonschema-form";
 import { JSONSchema6 } from "json-schema";
+import { useDelayedValidation } from "../hooks/use-delayed-validation";
 
 import "../../shared/styles/boostrap-3.3.7.css"; // necessary to style react-jsonschema-form
 import css from "../../shared/styles/authoring.scss";
@@ -14,9 +15,13 @@ export interface IBaseAuthoringProps<IAuthoredState> {
   uiSchema: UiSchema;
   // react-jsonschema-form additional fields.
   fields?: { [name: string]: Field };
+  validate?: (formData: IAuthoredState, errors: FormValidation) => FormValidation;
 }
 
-export const BaseAuthoring = <IAuthoredState,>({ authoredState, setAuthoredState, preprocessFormData, schema, uiSchema, fields }: IBaseAuthoringProps<IAuthoredState>) => {
+export const BaseAuthoring = <IAuthoredState,>({ authoredState, setAuthoredState, preprocessFormData, schema, uiSchema, fields, validate }: IBaseAuthoringProps<IAuthoredState>) => {
+  const formRef = useRef<Form<IAuthoredState>>(null);
+  const triggerDelayedValidation = useDelayedValidation({ formRef });
+
   const onChange = (event: IChangeEvent) => {
     let formData = event.formData;
     if (preprocessFormData) {
@@ -24,17 +29,28 @@ export const BaseAuthoring = <IAuthoredState,>({ authoredState, setAuthoredState
     }
     // Immediately save the data.
     setAuthoredState?.(formData);
+    if (validate) {
+      triggerDelayedValidation();
+    }
   };
+
+  useEffect(() => {
+    if (validate) {
+      // Initial validation.
+      triggerDelayedValidation();
+    }
+  }, []);
 
   return (
     <div className={css.authoring}>
       <Form
+        ref={formRef}
         schema={schema}
         uiSchema={uiSchema}
         formData={authoredState}
         onChange={onChange}
-        noValidate={true}
         fields={fields}
+        validate={validate}
       >
         {/* Children are used to render custom action buttons. We don't want any, */}
         {/* as form is saving and validating data live. */}
