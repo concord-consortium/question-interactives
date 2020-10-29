066e446c8834968efb4296d0a203840c0a810104
Unknown <emcelroy@concord.org>
2020-10-29 17:29:50 -0400

    Add carousel interactive.

---
+++ src/carousel/components/types.ts
@@ -0,0 +1,24 @@
+import {
+  IAuthoringInteractiveMetadata, IRuntimeInteractiveMetadata
+} from "@concord-consortium/lara-interactive-api";
+
+// Note that TS interfaces should match JSON schema. Currently there's no way to generate one from the other.
+// TS interfaces are not available in runtime in contrast to JSON schema.
+
+export interface IAuthoredState extends IAuthoringInteractiveMetadata {
+  version: number;
+  hint?: string;
+  subinteractives?: {
+    id: string;
+    url: string;
+    authoredState: any;
+  }[]
+}
+
+export interface IInteractiveState extends IRuntimeInteractiveMetadata {
+  subinteractiveStates: {
+    [id: string]: any;
+  },
+  currentSubinteractiveId: string;
+  submitted: boolean;
+}
--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,7 @@ module.exports = (env, argv) => {
     context: __dirname, // to automatically find tsconfig.json
     devtool: 'source-map',
     entry: {
+      'carousel': './src/carousel/index.tsx',
       'multiple-choice': './src/multiple-choice/index.tsx',
       'multiple-choice-alerts': './src/multiple-choice-alerts/index.tsx',
       'open-response': './src/open-response/index.tsx',
@@ -124,6 +125,11 @@ module.exports = (env, argv) => {
         filename: devMode ? "[name]/assets/index.css" : "[name]/assets/index.[hash].css"
       }),
       // HtmlWebpackPlugin and CopyWebpackPlugin will need to be configured in a similar way for all future question types.
+      new HtmlWebpackPlugin({
+        chunks: ['carousel'],
+        filename: 'carousel/index.html',
+        template: 'src/shared/index.html'
+      }),
       new HtmlWebpackPlugin({
         chunks: ['multiple-choice'],
         filename: 'multiple-choice/index.html',
