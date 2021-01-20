e62ed29ad37a3abd0c711fa8b97438e1248ae4ab
pjanik <janikpiotrek@gmail.com>
2021-01-20 13:51:25 +0100

    Update Interactive API Client

--- src/drag-and-drop/components/container.test.tsx
+++ src/drag-and-drop/components/container.test.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { mount } from "enzyme";
-import { Container, margin, marginLeft, marginTop } from "./container";
+import { margin, marginLeft, marginTop } from "./container";
 import { DraggableItemWrapper } from "./draggable-item-wrapper";
 import { ContainerWithDndProvider } from "./container-with-dnd-provider";
 import { DraggableItemPreview } from "./draggable-item-preview";
