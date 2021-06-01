0612a65d5f1ad95027f944ac5838873635c00964
eireland <eireland@concord.org>
2021-06-01 16:39:39 -0700

    Hides item value Moves item label to below the image

--- src/drag-and-drop/components/draggable-item.tsx
+++ src/drag-and-drop/components/draggable-item.tsx
@@ -10,9 +10,9 @@ export interface IProps {
 export const DraggableItem: React.FC<IProps> = ({ item }) => {
   return (
     <div className={`${css.draggableItem}`}>
+      {item.imageUrl &&
+        <img src={item.imageUrl} alt="draggable item" width={`${item.imageWidth}px`} height={`${item.imageHeight}px`}/>}
       <div className={`${css.itemLabel}`}>{item.itemLabel}</div>
-      <div className={`${css.itemValue}`}>{item.itemValue} {item.itemUnit}</div>
-      {item.imageUrl && <img src={item.imageUrl} alt="draggable item" width={`${item.imageWidth}px`} height={`${item.imageHeight}px`}/>}
     </div>
   );
 };
