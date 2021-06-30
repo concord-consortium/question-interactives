e8cbf57bf5c67aed0f2c2eeda20847e3f9da8d42
eireland <eireland@concord.org>
2021-06-30 16:24:12 -0700

    Initial work on adding the interactive to scale

--- src/full-screen/components/types.ts
+++ src/full-screen/components/types.ts
@@ -1,6 +1,31 @@
+import { IAuthoringInteractiveMetadata, IRuntimeInteractiveMetadata } from "@concord-consortium/lara-interactive-api";
+
 // Note that TS interfaces should match JSON schema. Currently there's no way to generate one from the other.
 // TS interfaces are not available in runtime in contrast to JSON schema.
+export interface IAuthoredState extends IAuthoringInteractiveMetadata {
+  version: 2;
+  hint?: string;
+  subinteractives?: {
+    id: string;
+    libraryInteractiveId: string;
+    authoredState: any;
+  }[]
+}
 
-export interface IAuthoredState {
-  version: number;
+export interface IInteractiveState extends IRuntimeInteractiveMetadata {
+  subinteractiveStates: {
+    [id: string]: any;
+  },
+  currentSubinteractiveId: string;
+  submitted?: boolean;
+}
+
+export interface IAuthoredStateV1 extends IAuthoringInteractiveMetadata {
+  version: 1;
+  hint?: string;
+  subinteractives?: {
+    id: string;
+    url: string;
+    authoredState: any;
+  }[]
 }
