3bba1e263ea48250957b73f7a7b45b06468925e9
eireland <eireland@concord.org>
2021-07-06 17:52:58 -0700

    Gets which interactive to wrap via query param Adds query-string library Removes querystring library Removes iframe authoring and iframe-runtime

--- src/full-screen/components/runtime.tsx
+++ src/full-screen/components/runtime.tsx
@@ -1,10 +1,8 @@
-import React, { useCallback, useEffect } from "react";
+import React, { useCallback, useEffect} from "react";
 import _screenfull from "screenfull";
 import queryString from "query-string";
 import { FullScreenButton } from "./full-screen-button";
 import { useForceUpdate } from "../../shared/hooks/use-force-update";
-import { IframeRuntime } from "./iframe-runtime";
-import { libraryInteractiveIdToUrl } from "../../shared/utilities/library-interactives";
 import { renderHTML } from "../../shared/utilities/render-html";
 
 import css from "./runtime.scss";
