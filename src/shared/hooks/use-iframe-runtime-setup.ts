4c5931bf11b62fa6794e96a1492dbdf362093129
eireland <eireland@concord.org>
2021-07-14 15:36:00 -0700

    moved initinteractive to a callback

--- src/shared/hooks/use-iframe-runtime-setup.ts
+++ src/shared/hooks/use-iframe-runtime-setup.ts
@@ -29,51 +29,51 @@ export const useIframeRuntimeSetup =
   // const setInteractiveStateRef = useRef<((state: any) => void)>(setInteractiveState);
   const phoneRef = useRef<IframePhone>();
 
-  useEffect (() => {
-    const initInteractive = () => {
-      const phone = phoneRef.current;
-      if (!phone) {
-        return;
-      }
-      phone.addListener("interactiveState", (newInteractiveState: any) => {
-        setInteractiveStateRef.current?.(newInteractiveState);
+  const initInteractive = useCallback(() => {
+    const phone = phoneRef.current;
+    if (!phone) {
+      return;
+    }
+    phone.addListener("interactiveState", (newInteractiveState: any) => {
+      setInteractiveStateRef.current?.(newInteractiveState);
+    });
+    phone.addListener("height", (newHeight: number) => {
+      setIframeHeight(newHeight);
+    });
+    phone.addListener("hint", (newHint: IHintRequest) => {
+      setHint(newHint.text || "");
+    });
+    phone.addListener("log", (logData: ILogRequest) => {
+      log(logData.action, logRequestData);
+    });
+    phone.post("initInteractive", {
+      mode: report ? "report" : "runtime",
+      authoredState,
+      // This is a trick not to depend on interactiveState.
+      interactiveState: interactiveStateRef.current
+    });
+    if (hasModal) {
+      phone.addListener("showModal", (modalOptions: IShowModal) => {
+        if (modalOptions.type === "dialog") {
+          // Note that dialog is very different from lightbox and alert - it depends on the interactive state.
+          // It'd require quite complex code to handle it. Lightbox only displays provided URL (for example hi-res image)
+          // and there's no associated state. Alert shows provided message. However, dialog renders complete
+          // interactive, so it needs to receive correct interactive state. Carousel would need to capture provided URL,
+          // modify it so it still points to carousel interactive instead of the subinteractive, send it to parent,
+          // open itself in dialog, hide carousel UI and provide correct state to the subinteractive. It's doable, but
+          // not necessary at this point, as none of the carousel interactives uses "dialog" modal type.
+          window.alert("Dialog is not supported within carousel.");
+          return;
+        }
+        showModal(modalOptions);
       });
-      phone.addListener("height", (newHeight: number) => {
-        setIframeHeight(newHeight);
+      phone.addListener("closeModal", (modalOptions: ICloseModal) => {
+        closeModal(modalOptions);
       });
-      phone.addListener("hint", (newHint: IHintRequest) => {
-        setHint(newHint.text || "");
-      });
-      phone.addListener("log", (logData: ILogRequest) => {
-        log(logData.action, logRequestData);
-      });
-      phone.post("initInteractive", {
-        mode: report ? "report" : "runtime",
-        authoredState,
-        // This is a trick not to depend on interactiveState.
-        interactiveState: interactiveStateRef.current
-      });
-      if (hasModal) {
-        phone.addListener("showModal", (modalOptions: IShowModal) => {
-          if (modalOptions.type === "dialog") {
-            // Note that dialog is very different from lightbox and alert - it depends on the interactive state.
-            // It'd require quite complex code to handle it. Lightbox only displays provided URL (for example hi-res image)
-            // and there's no associated state. Alert shows provided message. However, dialog renders complete
-            // interactive, so it needs to receive correct interactive state. Carousel would need to capture provided URL,
-            // modify it so it still points to carousel interactive instead of the subinteractive, send it to parent,
-            // open itself in dialog, hide carousel UI and provide correct state to the subinteractive. It's doable, but
-            // not necessary at this point, as none of the carousel interactives uses "dialog" modal type.
-            window.alert("Dialog is not supported within carousel.");
-            return;
-          }
-          showModal(modalOptions);
-        });
-        phone.addListener("closeModal", (modalOptions: ICloseModal) => {
-          closeModal(modalOptions);
-        });
-      }
-    };
+    }
+  },[]);
 
+  useEffect (() => {
     if (iframeRef.current) {
       // Reload the iframe.
       iframeRef.current.src = url;
