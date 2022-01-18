ddad6600a617126c09b938faaea46d6383579999
eireland <eireland@concord.org>
2022-01-18 12:47:12 -0800

    Opens prompt links in a new tab

--- src/shared/utilities/render-html.ts
+++ src/shared/utilities/render-html.ts
@@ -15,6 +15,14 @@ interface DomElement {
 // eslint-disable-next-line @typescript-eslint/ban-types
 export type ParseHTMLReplacer = (domNode: DomElement) => JSX.Element | object | void | undefined | null | false;
 
+DOMPurify.setConfig({ ADD_ATTR: ['target'] });
 export function renderHTML(html: string, replace?: ParseHTMLReplacer) {
-  return parse(DOMPurify.sanitize(html || ""), { replace });
+  let newHtml;
+  if (html.includes("<a")) {
+    const position = html.indexOf("<a") + 2;
+    newHtml = [html.slice(0, position), " target=\"_blank\"", html.slice(position)].join("");
+  } else {
+    newHtml = html;
+  }
+  return parse(DOMPurify.sanitize(newHtml || ""), { replace });
 }
