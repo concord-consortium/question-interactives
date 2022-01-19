1edc28cf11fa5247ebdd4ae2294ee74036866312
Evangeline Ireland <eireland@concord.org>
2022-01-19 10:39:39 -0500

    Opens prompt links in a new tab (#150)
    * Opens prompt links in a new tab
    
    * PR fix that usus DOMPurify's afterSanitizeAttributes hook

--- src/shared/utilities/render-html.ts
+++ src/shared/utilities/render-html.ts
@@ -15,6 +15,16 @@ interface DomElement {
 // eslint-disable-next-line @typescript-eslint/ban-types
 export type ParseHTMLReplacer = (domNode: DomElement) => JSX.Element | object | void | undefined | null | false;
 
+DOMPurify.setConfig({ ADD_ATTR: ['target'] });
+DOMPurify.addHook("afterSanitizeAttributes", function(node) {
+  // Fix `a` elements:
+  // - add `target="_blank"
+  // - add `rel="noopener noreferrer"` to prevent https://www.owasp.org/index.php/Reverse_Tabnabbing
+  if (node.tagName.toLowerCase() === "a") {
+    node.setAttribute("target", "_blank");
+    node.setAttribute("rel", "noopener noreferrer");
+  }
+});
 export function renderHTML(html: string, replace?: ParseHTMLReplacer) {
   return parse(DOMPurify.sanitize(html || ""), { replace });
 }
