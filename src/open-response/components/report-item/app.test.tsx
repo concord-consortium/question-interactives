880ede12b87d907cccedea46a2cfcd11f0d173ce
Unknown <emcelroy@concord.org>
2022-04-01 16:20:39 -0400

    Support multiple answer types and send them to the Portal Report
    PT Story: https://www.pivotaltracker.com/story/show/181516279
    
    [#181516279]
    
    These changes add an open response report item.

---
+++ src/open-response/components/report-item/app.test.tsx
@@ -0,0 +1,71 @@
+import React from "react";
+import { act, render } from "@testing-library/react";
+import { useInitMessage } from "@concord-consortium/lara-interactive-api";
+import { AppComponent } from "./app";
+import { IAuthoredState, IInteractiveState } from "../types";
+
+jest.mock("./report-item", () => ({
+  ReportItemComponent: () => {
+    return <div>MockReportItemComponent</div>;
+  }
+}));
+
+jest.mock("@concord-consortium/lara-interactive-api", () => ({
+  useInitMessage: jest.fn(),
+  useAutoSetHeight: jest.fn(),
+}));
+
+const useInitMessageMock = useInitMessage as jest.Mock;
+
+const authoredState = {
+  version: 1,
+  questionType: "open_response",
+  prompt: "Test prompt",
+  hint: "hint",
+  required: false,
+  defaultAnswer: ""
+} as IAuthoredState;
+
+const interactiveState = {
+  answerType: "open_response_answer",
+  answerText: "Test answer",
+} as IInteractiveState;
+
+describe("Open response question report item", () => {
+  it("renders a 'Loading...' message if initMessage is not yet defined", async () => {
+    const { container } = render(<AppComponent />);
+    expect(container).toBeDefined();
+    await act(async () => {
+      expect(container.innerHTML).toEqual(expect.stringContaining("Loading..."));
+    });
+  });
+
+  it("renders an error message if mode is not reportItem", async () => {
+    useInitMessageMock.mockReturnValue({
+      version: 1,
+      mode: "authoring",
+      authoredState
+    });
+
+    const { container } = render(<AppComponent />);
+    expect(container).toBeDefined();
+    await act(async () => {
+      expect(container.innerHTML).toEqual(expect.stringContaining("This interactive is only available in 'reportItem' mode but 'authoring' was given."));
+    });
+  });
+
+  it("returns the report item component if initMessage is set and mode is reportItem", async () => {
+    useInitMessageMock.mockReturnValue({
+      version: 1,
+      mode: "reportItem",
+      authoredState,
+      interactiveState
+    });
+
+    const { container } = render(<AppComponent />);
+    expect(container).toBeDefined();
+    await act(async () => {
+      expect(container.innerHTML).toEqual(expect.stringContaining("MockReportItemComponent"));
+    });
+  });
+});
