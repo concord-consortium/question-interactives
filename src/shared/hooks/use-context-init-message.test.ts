181c1912945ea577fdc900af9d1f6040d4a5be73
Unknown <emcelroy@concord.org>
2022-04-13 14:21:07 -0400

    Add comment and test.

---
+++ src/shared/hooks/use-context-init-message.test.ts
@@ -0,0 +1,12 @@
+import { renderHook } from "@testing-library/react-hooks";
+import { useContextInitMessage } from "./use-context-init-message";
+
+describe("useContextInitMessage", () => {
+  it("should return an error if it is called from outside a context", () => {
+    const HookWrapper = () => {
+      useContextInitMessage();
+    };
+    const { result } = renderHook(HookWrapper);
+    expect(result.error.message).toBe("useContextInitMessage must be used within a <InitMessageContext.Provider>");
+  });
+});
