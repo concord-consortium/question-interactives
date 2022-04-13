d4e30a9620b3b2f73b1396c80ea23bed53dc732d
Unknown <emcelroy@concord.org>
2022-04-13 14:21:07 -0400

    Add comment and test.

--- src/shared/hooks/use-context-init-message.ts
+++ src/shared/hooks/use-context-init-message.ts
@@ -5,6 +5,9 @@ export const InitMessageContext = createContext<IInitInteractive | null | undefi
 
 export const useContextInitMessage = () => {
   const initMessage = useContext(InitMessageContext);
+  // initMessage will be undefined unless a value is supplied by a context provider.
+  // useInitMessage in the interactive client returns null until the init message is ready,
+  // after which it returns an object of type IInitInteractive
   if (initMessage === undefined) {
     throw new Error("useContextInitMessage must be used within a <InitMessageContext.Provider>");
   }
