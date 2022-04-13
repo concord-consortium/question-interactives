37818523c96fb3a402fd37390aa4da228d84149b
Unknown <emcelroy@concord.org>
2022-04-13 10:00:32 -0400

    Throw error if useContextInitMessage is not called from within a context

--- src/shared/hooks/use-context-init-message.ts
+++ src/shared/hooks/use-context-init-message.ts
@@ -1,9 +1,12 @@
 import { createContext, useContext } from "react";
 import { IInitInteractive } from "@concord-consortium/lara-interactive-api";
 
-export const InitMessageContext = createContext<IInitInteractive | null>(null);
+export const InitMessageContext = createContext<IInitInteractive | null | undefined>(undefined);
 
 export const useContextInitMessage = () => {
   const initMessage = useContext(InitMessageContext);
+  if (initMessage === undefined) {
+    throw new Error("useContextInitMessage must be used within a <InitMessageContext.Provider>");
+  }
   return initMessage;
 };
