a80b259facb6b12f24942dfc59a32b24b58022ea
pjanik <janikpiotrek@gmail.com>
2022-11-04 14:42:41 +0100

    Fix image upload
    [#183721375]

--- src/labbook/components/styled-file-input.tsx
+++ src/labbook/components/styled-file-input.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import css from "./styled-file-input.scss";
 import classNames from "classnames";
 
@@ -18,6 +18,16 @@ export interface IStyledFileInputProps {
 export const StyledFileInput: React.FC<IStyledFileInputProps> = (props) => {
   const {children, onChange, buttonClass} = props;
   const classes = classNames(buttonClass);
+  const fileInputRef = useRef<HTMLInputElement>(null);
+
+  const handleClick = () => {
+    // Reset current file input value as soon as user clicks on the button. Otherwise, user won't be able to upload
+    // the same file again. This is necessary in Labbook after user deletes the current thumbnail, and might be
+    // generally useful in other scenarios. See: https://www.pivotaltracker.com/story/show/183721375
+    if (fileInputRef.current) {
+      fileInputRef.current.value = "";
+    }
+  };
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const file = event.target.files?.[0];
@@ -32,7 +42,7 @@ export const StyledFileInput: React.FC<IStyledFileInputProps> = (props) => {
         data-testid="upload-btn">
           {children}
       </label>
-      <input className={css.hidden} id="file-upload" type="file" onChange={handleChange}/>
+      <input ref={fileInputRef} className={css.hidden} id="file-upload" type="file" onClick={handleClick} onChange={handleChange}/>
     </>
   );
 };
