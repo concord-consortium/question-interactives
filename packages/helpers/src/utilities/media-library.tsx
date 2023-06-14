d0fc0c72f21b7e9c7c8a52124ff582e0da6840fe
Doug Martin <dmartin@concord.org>
2023-06-14 15:01:58 -0400

    feat: Authoring setting for upload from media library [PT-185344721]

--- packages/helpers/src/utilities/media-library.tsx
+++ packages/helpers/src/utilities/media-library.tsx
@@ -1,6 +1,12 @@
 import React from "react";
 
-type ExportToMediaLibraryAuthoringPropsOptions = {url: string, type: "image", caption: string|boolean}
+type ExportToMediaLibraryAuthoringPropsOptions = {
+  exportLabel: string;
+  url: string;
+  type: "image";
+  caption: string|boolean;
+  addAllowUpload?: boolean;
+}
 
 export const ExportToMediaLibraryWidget = (props: any) => {
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => props.onChange(e.target.checked);
@@ -9,18 +15,32 @@ export const ExportToMediaLibraryWidget = (props: any) => {
     <div className="checkbox">
       <label>
         <input type="checkbox" id={props.id} checked={!!props.value} onChange={handleChange} />
-        <span>Export to Media Library</span>
+        <span>{props.schema.customLabel}</span>
+      </label>
+    </div>
+  );
+};
+
+export const AllowUploadFromMediaLibraryWidget = (props: any) => {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => props.onChange(e.target.checked);
+
+  return (
+    <div className="checkbox">
+      <label>
+        <input type="checkbox" id={props.id} checked={!!props.value} onChange={handleChange} />
+        <span>Allow Student to Upload from Media Library</span>
       </label>
     </div>
   );
 };
 
 export const exportToMediaLibraryAuthoringProps = (options: ExportToMediaLibraryAuthoringPropsOptions) => {
-  const {url, type, caption} = options;
+  const {exportLabel, url, type, caption, addAllowUpload} = options;
 
   const schemaProperties: any = {
     exportToMediaLibrary: {
-      title: "Media Library",
+      title: "Export To Media Library",
+      customLabel: `Export ${exportLabel} To Media Library`,
       type: "boolean"
     },
     exportToMediaLibraryType: {
@@ -57,6 +77,18 @@ export const exportToMediaLibraryAuthoringProps = (options: ExportToMediaLibrary
       "ui:widget": "hidden"
     };
     uiOrder.push("exportToMediaLibraryCaptionField");
+  }
+
+  if (addAllowUpload) {
+    schemaProperties.allowUploadFromMediaLibrary = {
+      title: "Upload From Media Library",
+      type: "boolean"
+    };
+    uiSchema.allowUploadFromMediaLibrary ={
+      "ui:widget": AllowUploadFromMediaLibraryWidget,
+      "ui:help": "Check this option to allow students to upload images from this activity's Media Library",
+    };
+    uiOrder.push("allowUploadFromMediaLibrary");
   }
 
   return {schemaProperties, uiSchema, uiOrder};
