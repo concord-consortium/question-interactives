9ab5c7caf0b0f1af3194aec6d73a0b283048d77f
Ethan McElroy <emcelroy@concord.org>
2025-09-17 15:37:17 -0400

    feat: add Blockly

--- packages/blockly/src/components/blockly.tsx
+++ packages/blockly/src/components/blockly.tsx
@@ -1,6 +1,7 @@
-import React, { useEffect } from "react";
-
 import { IRuntimeQuestionComponentProps } from "@concord-consortium/question-interactives-helpers/src/components/base-question-app";
+import { inject } from "blockly";
+import React, { useEffect, useRef } from "react";
+
 import { IAuthoredState, IInteractiveState } from "./types";
 
 import css from "./blockly.scss";
@@ -9,14 +10,29 @@ interface IProps extends IRuntimeQuestionComponentProps<IAuthoredState, IInterac
 
 export const BlocklyComponent: React.FC<IProps> = ({ authoredState, interactiveState, setInteractiveState, report }) => {
   const { config } = authoredState;
+  const errorRef = useRef<Error | null>(null);
+  const blocklyDivRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    // TODO: update blockly based on config changes
+    if (!config) {
+      errorRef.current = new Error("Enter a toolbox configuration to see Blockly.");
+      return;
+    }
+
+    if (blocklyDivRef.current) {
+      try {
+        inject(blocklyDivRef.current, {toolbox: JSON.parse(config)});
+        errorRef.current = null;
+      } catch (e) {
+        errorRef.current = e;
+      }
+    }
   }, [config]);
 
   return (
     <div className={css.blockly}>
-      {JSON.stringify(config, null, 2)}
+      {errorRef.current && <div className={css.error}>Error loading Blockly: {errorRef.current.message}</div>}
+      <div className={css.blocklyDiv} ref={blocklyDivRef}></div>
     </div>
   );
 };
