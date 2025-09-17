2b6bf46e3a5beb4c3cf5c7413568e206ef90f73a
Doug Martin <dmartin@concord.org>
2025-09-17 14:27:33 -0400

    Fixed tests

--- packages/blockly/src/components/runtime.test.tsx
+++ packages/blockly/src/components/runtime.test.tsx
@@ -1,14 +1,13 @@
 import React from "react";
 import { mount } from "enzyme";
 import { Runtime } from "./runtime";
-import { Bar } from "react-chartjs-2";
 import { DemoAuthoredState } from "./types";
 import { InitMessageContext } from "@concord-consortium/question-interactives-helpers/src/hooks/use-context-init-message";
-import { useInitMessage, log } from "@concord-consortium/lara-interactive-api";
+import { useInitMessage } from "@concord-consortium/lara-interactive-api";
 import { DynamicTextTester } from "@concord-consortium/question-interactives-helpers/src/utilities/dynamic-text-tester";
+import { BlocklyComponent } from "./blockly";
 
 const useInitMessageMock = useInitMessage as jest.Mock;
-const logMock = log as jest.Mock;
 
 jest.mock("@concord-consortium/lara-interactive-api", () => ({
   useInitMessage: jest.fn(),
@@ -23,12 +22,12 @@ const initMessage = {
 };
 useInitMessageMock.mockReturnValue(initMessage);
 
-describe("Graph runtime", () => {
+describe("Blockly runtime", () => {
   beforeEach(() => {
     useInitMessageMock.mockClear();
   });
 
-  it("renders a graph with the correct props", () => {
+  it("renders a blockly component with the correct props", () => {
     const wrapper = mount(
       <InitMessageContext.Provider value={initMessage as any}>
         <DynamicTextTester>
@@ -36,92 +35,6 @@ describe("Graph runtime", () => {
         </DynamicTextTester>
       </InitMessageContext.Provider>
     );
-    expect(wrapper.find(Bar).length).toEqual(1);
-
-    // NOTE: chart.js has an extensive test suite so the only thing we need
-    // to test is if our mapping of the authored state to the props is working
-    const props = wrapper.find(Bar).props() as any;
-    delete props.options.plugins.chartInfo.callback;
-    expect(props).toEqual({
-      "options": {
-        "animation": false,
-        "responsive": true,
-        "scales": {
-          "x": {
-            "title": {
-              "display": false,
-            },
-            "ticks": {
-              "font": {
-                "size": 20,
-                "family": "'Lato', sans-serif",
-                "weight": undefined,
-              },
-              "color": "#3f3f3f"
-            }
-          },
-          "y": {
-            "type": "linear",
-            "title": {
-              "display": false,
-            },
-            "ticks": {
-              "stepSize": 10
-            },
-            "beginAtZero": true,
-            "max": 100
-          }
-        },
-        "plugins": {
-          "legend": {
-            "display": false
-          },
-          "title": {
-            "display": false,
-          },
-          "tooltip": {
-            "enabled": false
-          },
-          "chartInfo": {
-            // "callback": <-- deleted since this is a function variable
-          }
-        }
-      },
-      "data": {
-        "labels": [
-          "Winter",
-          "Spring",
-          "Summer",
-          "Fall"
-        ],
-        "datasets": [
-          {
-            "data": [
-              25,
-              0,
-              75,
-              50
-            ],
-            "backgroundColor": [
-              "#EA6D2F",
-              "#FFC320",
-              "#2DA343",
-              "#6FC6DA"
-            ]
-          }
-        ]
-      }
-    });
-  });
-
-  it("logs a init message at startup", () => {
-    mount(
-      <InitMessageContext.Provider value={initMessage as any}>
-        <DynamicTextTester>
-          <Runtime authoredState={DemoAuthoredState} />
-        </DynamicTextTester>
-      </InitMessageContext.Provider>
-    );
-    expect(logMock).toHaveBeenCalledWith("init bar chart", {barChart: DemoAuthoredState});
+    expect(wrapper.find(BlocklyComponent).length).toEqual(1);
   });
 });
