f1636e528d7e97bc73f81d48479c3b8345d1725f
Teale Fristoe <fristoe@gmail.com>
2025-09-24 19:56:13 -0700

    Set up agent-simulation to read blocklyCode from another sim.

--- packages/agent-simulation/src/components/runtime.tsx
+++ packages/agent-simulation/src/components/runtime.tsx
@@ -1,10 +1,18 @@
-import React from "react";
-import { IRuntimeQuestionComponentProps } from "@concord-consortium/question-interactives-helpers/src/components/base-question-app";
-import { renderHTML } from "@concord-consortium/question-interactives-helpers/src/utilities/render-html";
+import React, { useEffect, useState } from "react";
 import { DynamicText } from "@concord-consortium/dynamic-text";
+import {
+  addLinkedInteractiveStateListener, removeLinkedInteractiveStateListener
+} from "@concord-consortium/lara-interactive-api";
+import {
+  IRuntimeQuestionComponentProps
+} from "@concord-consortium/question-interactives-helpers/src/components/base-question-app";
+import {
+  useLinkedInteractiveId
+} from "@concord-consortium/question-interactives-helpers/src/hooks/use-linked-interactive-id";
+import { renderHTML } from "@concord-consortium/question-interactives-helpers/src/utilities/render-html";
 
-import { DefaultAuthoredState, IAuthoredState, IInteractiveState } from "./types";
 import { AgentSimulationComponent } from "./agent-simulation";
+import { DefaultAuthoredState, IAuthoredState, IInteractiveState } from "./types";
 
 import css from "./runtime.scss";
 
@@ -12,10 +20,33 @@ interface IProps extends IRuntimeQuestionComponentProps<IAuthoredState, IInterac
 
 export const Runtime: React.FC<IProps> = ({ authoredState, interactiveState, setInteractiveState, report }) => {
   authoredState = {...DefaultAuthoredState, ...authoredState};
+  const [blocklyCode, setBlocklyCode] = useState<string>("");
+  const dataSourceInteractive1 = useLinkedInteractiveId("dataSourceInteractive1");
+
+  useEffect(() => {
+    if (!dataSourceInteractive1) return;
+
+    const listener = (newLinkedIntState: IInteractiveState | undefined) => {
+      const newBlocklyCode = newLinkedIntState && "blocklyCode" in newLinkedIntState && newLinkedIntState.blocklyCode;
+      if (typeof newBlocklyCode === "string") {
+        setBlocklyCode(newBlocklyCode);
+      } else {
+        setBlocklyCode("");
+      }
+
+      const options = { interactiveItemId: dataSourceInteractive1 };
+      addLinkedInteractiveStateListener<any>(listener, options);
+    };
+
+    return () => {
+      removeLinkedInteractiveStateListener<any>(listener);
+    };
+  }, [dataSourceInteractive1]);
 
   return (
-    <div className={css.barGraph}>
+    <div className={css.agentSimulation}>
       {authoredState.prompt && <div><DynamicText>{renderHTML(authoredState.prompt)}</DynamicText></div>}
+      <p>{blocklyCode}</p>
       <AgentSimulationComponent
         authoredState={authoredState}
         interactiveState={interactiveState}
