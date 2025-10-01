bb9873a46e73419234844493203c419254e1afc4
Teale Fristoe <fristoe@gmail.com>
2025-09-30 20:16:45 -0700

    Start fleshing out predator-prey sim.

--- packages/agent-simulation/src/components/sims/predator-prey-model.ts
+++ packages/agent-simulation/src/components/sims/predator-prey-model.ts
@@ -1,28 +1,71 @@
 import * as AA from "@gjmcn/atomic-agents";
 
+const sheepEnergy = 10;
+const wolfEnergy = 10;
+
 export const sim = new AA.Simulation({
-  width: 600,
-  height: 400,
+  width: 450,
+  height: 450,
   gridStep: 25
 });
 
-// circles
+// sheep
 sim.populate({
-  n: 200,
-  radius: 4,
-  setup: (ac: any) => ac.vel = AA.Vector.randomAngle(2)
+  n: 50,
+  radius: 3,
+  setup: (ac: any) => {
+    ac.vel = AA.Vector.randomAngle(1);
+    ac.vis({
+      tint: "0xffffff"
+    });
+    ac.label("sheep", true);
+    ac.state = { energy: sheepEnergy };
+    ac.updateState = () => {
+      ac.state.energy = ac.state.energy - 0.01;
+      if (ac.state.energy <= 0) {
+        ac.remove();
+      }
+    };
+  }
 });
+const sheep = sim.withLabel("sheep");
 
-// circles bounce off each other
-sim.interaction.set('circle-bounce', {
-  group1: sim.actors,
-  behavior: 'bounce',
-  speed: 2  // fixed speed circles
+// wolves
+sim.populate({
+  n: 10,
+  radius: 4,
+  setup: (ac: any) => {
+    ac.vel = AA.Vector.randomAngle(1.5);
+    ac.vis({
+      tint: "0x333333"
+    });
+    ac.label("wolf", true);
+    ac.state = { energy: wolfEnergy };
+    ac.updateState = () => {
+      ac.state.energy = ac.state.energy - 0.01;
+      if (ac.state.energy <= 0) {
+        ac.remove();
+      }
+    };
+  }
+});
+const wolves = sim.withLabel("wolf");
+
+sim.interaction.set("wolf-eats-sheep", {
+  group1: wolves,
+  group2: sheep,
+  behavior: "custom",
+  force: (w: any, s: any) => {
+    // wolf eats sheep
+    w.state.energy = w.state.energy + sheepEnergy / 2;
+    s.remove();
+    return AA.Vector.randomAngle(1.5);
+  }
 });
 
 // circles bounce off the simulation boundary
-sim.interaction.set('boundary-bounce', {
+sim.interaction.set("boundary-bounce", {
   group1: sim,
   group2: sim.actors,
-  behavior: 'bounce'
+  behavior: "bounce"
 });
