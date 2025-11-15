c4f7b74566bf0e3f3d73e265b2ee02b02c21ba76
Teale Fristoe <fristoe@gmail.com>
2025-11-14 16:17:51 -0800

    Use css to style slider.

--- packages/agent-simulation/src/widgets/slider-widget.tsx
+++ packages/agent-simulation/src/widgets/slider-widget.tsx
@@ -43,35 +43,6 @@ const SliderWidget = observer(function SliderWidget({ data, defaultValue, global
         max={max}
         onChange={handleChange}
         value={value}
-        // It would be better to define styles in css, but I couldn't figure out how to do it in the qi repo
-        styles={{
-          handle: {
-            position: "absolute",
-            top: "5px",
-            width: "15px",
-            height: "15px",
-            border: "solid 2px",
-            backgroundColor: "#fff",
-            borderRadius: "50%",
-            cursor: "grab",
-            touchAction: "pan-x",
-            zIndex: 2
-          },
-          rail: {
-            position: "absolute",
-            width: "95%",
-            height: "2px",
-            backgroundColor: "#949494"
-          },
-          track: {
-            position: "relative",
-            width: "95%",
-            height: "4px",
-            top: "-1px",
-            backgroundColor: "#545454",
-            borderRadius: "1px",
-          }
-        }}
       />
     </div>
   );
