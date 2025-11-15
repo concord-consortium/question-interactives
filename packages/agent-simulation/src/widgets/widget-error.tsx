797ca1a2bc9b33991256625e3c63bd68342356ee
Teale Fristoe <fristoe@gmail.com>
2025-11-14 16:08:18 -0800

    PR review feedback.

--- packages/agent-simulation/src/widgets/widget-error.tsx
+++ packages/agent-simulation/src/widgets/widget-error.tsx
@@ -5,6 +5,7 @@ import css from "./widget-error.scss";
 interface IWidgetErrorProps {
   message: string;
 }
+
 export function WidgetError({ message }: IWidgetErrorProps) {
   return <div className={css.widgetError}>{message}</div>;
 }
