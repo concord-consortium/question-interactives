d2362a2038b80b30691787e2c4172cc5a36988a7
Ethan McElroy <emcelroy@concord.org>
2025-11-17 16:02:20 -0500

    fix: duplicate key warning for widgets

--- packages/agent-simulation/src/components/widgets.tsx
+++ packages/agent-simulation/src/components/widgets.tsx
@@ -10,12 +10,12 @@ export function Widgets({ sim }: IWidgetProps) {
 
   return (
     <div className="widgets-container">
-      {sim.widgets.map(widget => {
+      {sim.widgets.map((widget, index) => {
         const WidgetComponent = widgetData[widget.type]?.component;
 
         if (!WidgetComponent) return null;
 
-        return <WidgetComponent key={widget.globalKey} {...widget} sim={sim} />;
+        return <WidgetComponent key={`${widget.globalKey}-${widget.type}-${index}`} {...widget} sim={sim} />;
       })}
     </div>
   );
