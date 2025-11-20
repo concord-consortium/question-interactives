84ec94e6c1344f4f22d704262cb60204966b9535
Teale Fristoe <fristoe@gmail.com>
2025-11-20 10:57:51 -0800

    QI-67 Flesh Out On Mouse Click (#426)
    * Set up adding agents via clicking.
    
    * Create new agents at mouse position rather than at random position if possible.

--- packages/blockly/src/sims/predator-prey-string.ts
+++ packages/blockly/src/sims/predator-prey-string.ts
@@ -134,6 +134,15 @@ function setup() {
   create_wolves(10);
 }
 
+// Agents added outside of the sim tick function (like onClick) do not get added to the renderer,
+// so instead of adding agents directly, we create a queue and add them in beforeTick.
+let actorsToAdd = [];
+
+sim.beforeTick = () => {
+  actorsToAdd.forEach(a => a.addTo(sim));
+  actorsToAdd = [];
+}
+
 sim.afterTick = () => {
   sim.squares.forEach(square => {
     // Grow grass
@@ -160,7 +169,6 @@ sim.afterTick = () => {
     const reproduceChance = a.label("sheep") ? sheepReproduceChance : wolfReproduceChance;
     if (Math.random() < reproduceChance) {
       const addFunction = a.label("sheep") ? create_a_sheep : create_a_wolf;
-      // const color = a.label("sheep") ? sheepColor : wolfColor;
       addFunction({ energy: a.state.energy / 2, x: a.x, y: a.y });
       a.state.energy = a.state.energy / 2;
     }
@@ -210,10 +218,10 @@ function create_a_sheep(props) {
   agent.vis({ image: sheepImage, tint: color });
   agent.label("sheep", true);
   agent.state = { energy: energy ?? sheepEnergy };
-  agent.x = x ?? Math.random() * sim.width;
-  agent.y = y ?? Math.random() * sim.height;
+  agent.x = x ?? globals.get("mouseX") ?? Math.random() * sim.width;
+  agent.y = y ?? globals.get("mouseY") ?? Math.random() * sim.height;
 
-  agent.addTo(sim);
+  actorsToAdd.push(agent);
   globals.set("sheepCount", globals.get("sheepCount") + 1);
   return agent;
 }
@@ -233,10 +241,10 @@ function create_a_wolf(props) {
   agent.vis({ image: wolfImage, tint: color ?? "0x333333" });
   agent.label("wolves", true);
   agent.state = { energy: energy ?? wolfEnergy };
-  agent.x = x ?? Math.random() * sim.width;
-  agent.y = y ?? Math.random() * sim.height;
+  agent.x = x ?? globals.get("mouseX") ?? Math.random() * sim.width;
+  agent.y = y ?? globals.get("mouseY") ?? Math.random() * sim.height;
 
-  agent.addTo(sim);
+  actorsToAdd.push(agent);
   globals.set("wolfCount", globals.get("wolfCount") + 1);
   return agent;
 };
@@ -255,4 +263,15 @@ sim.interaction.set("boundary-bounce", {
 });
 
 setup();
+
+function _onClick(event) {
+  globals.set("mouseX", event.data.global.x);
+  globals.set("mouseY", event.data.global.y);
+  onClick(event);
+}
+
+sim.vis({
+  background: true,
+  click: _onClick
+});
 `;
