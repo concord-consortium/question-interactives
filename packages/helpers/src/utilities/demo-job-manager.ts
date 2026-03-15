e0616f271c69123c060f9f808e6497f7c59f9b05
Doug Martin <dmartin@concord.org>
2026-03-15 14:12:33 -0400

    Remove TODO comment

--- packages/helpers/src/utilities/demo-job-manager.ts
+++ packages/helpers/src/utilities/demo-job-manager.ts
@@ -1,5 +1,3 @@
-// TODO: Replace with proper typed import once @concord-consortium/interactive-api-host is published
-// with JobManager. Currently using npm link to the local lara build.
 import { JobManager } from "@concord-consortium/interactive-api-host";
 import { MockJobExecutor } from "./mock-job-executor";
 
